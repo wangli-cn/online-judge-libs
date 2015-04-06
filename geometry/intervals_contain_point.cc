da6eecfa774a618262b39fb6f026dfea9906929c
Paul Meng <mno2@mno2.org>
2015-04-06 10:28:20 +0800

    Add interval tree use case

---
+++ geometry/intervals_contain_point.cc
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include "utils/interval_tree.h"
+
+int main(void)
+{
+    std::vector<int> pos {0, 8, 10, 11, 1000, 1006, 1009};
+    IntervalTree<int> T(pos);
+
+    T.insert(Interval(8, 10), 1);
+    T.insert(Interval(8, 1000), 2);
+    auto&& output = T.query(9);
+
+    std::sort(output.begin(), output.end());
+    std::copy(output.begin(), output.end(), std::ostream_iterator<int>(std::cout, " "));
+    return 0;
+}
