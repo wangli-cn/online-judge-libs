3f128c93d45e7a9df2a96cb3e79d615f7393730c
Paul Meng <mno2@mno2.org>
2015-04-06 10:23:55 +0800

    Move bipartite_matching to its own dir

--- graph_theory/bipartite_matching/bipartite_matching.cc
+++ graph_theory/bipartite_matching/bipartite_matching.cc
@@ -1,22 +1,8 @@
 #include <iostream>
 #include <vector>
+#include "utils/graph.h"
 
-using namespace std;
-
-#define REP(i,n) for(int i = 0; i < (int)n; ++i)
-#define FOR(i,c) for(__typeof((c).begin()) i = (c).begin(); i != (c).end(); ++i)
-
-struct Edge 
-{
-    int src, dst;
-    Edge(int src, int dst) 
-        :src(src), dst(dst) {}
-};
-
-typedef vector<Edge> Edges;
-typedef vector<Edges> Graph;
-
-bool augment(const Graph &g, int u, vector<int> &matchTo, vector<bool> &visited)
+bool augment(const Graph &g, int u, std::vector<int> &matchTo, std::vector<bool> &visited)
 {
     if (u < 0) return true;
     FOR(e, g[u]) if (!visited[e->dst]) {
@@ -33,15 +19,17 @@ bool augment(const Graph &g, int u, vector<int> &matchTo, vector<bool> &visited)
 int bipartiteMatching(const Graph &g, int L, Edges &matching)
 {
     const int n = g.size();
-    vector<int> matchTo(n, -1);
+    std::vector<int> matchTo(n, -1);
     int match = 0;
+
     REP(u, L) {
-        vector<bool> visited(n);
+        std::vector<bool> visited(n);
         if (augment(g, u, matchTo, visited)) ++match;
     }
 
     REP(u, L) if (matchTo[u] >= 0) //make explicit matching
         matching.push_back(Edge(u, matchTo[u]));
+
     return match;
 }
 
