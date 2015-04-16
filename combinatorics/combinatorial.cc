d6e2f68054031644ef771b25d34480841bb556a5
Paul Meng <mno2@mno2.org>
2015-04-16 09:11:10 +0800

    Organize combinatorics dir

--- combinatorics/combinatorial.cc
+++ combinatorics/combinatorial.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+
 const int MAXN = 100;
 
 long long C(int n, int r)
@@ -7,9 +9,14 @@ long long C(int n, int r)
         c[i] = 1LL;
 
     for (int i = 0; i <= n-r; ++i) {
-        for (int j = 1; j <= r; ++j) 
+        for (int j = 1; j <= r; ++j)
             c[j] += c[j-1];
     }
 
     return c[r];
+}
+
+int main()
+{
+    return 0;
 }
