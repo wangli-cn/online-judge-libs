a1bc73a91ab75f0c23911c14998205e379514cd4
Paul Meng <mno2@mno2.org>
2015-04-17 22:49:41 +0800

    Field testing joseph

--- games/joseph.cc
+++ games/joseph.cc
@@ -1,7 +1,21 @@
-int joseph_problem(int N, int M)
+// Field Test: http://tioj.ck.tp.edu.tw/problems/1004
+//
+#include <iostream>
+
+int joseph(int n, int m)
 {
     int i = 0;
-    for (int k = 1; k <= N; ++k) 
-        i = (i+M) % k;
+    for (int k = 1; k <= n; k++) {
+        i = (i+m) % k;
+    }
     return i;
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::cout << joseph(n, 2)+1 << std::endl;
+
+    return 0;
 }
