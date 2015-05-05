579afb20037976be1ae63ccbe924c26022c49e33
Paul Meng <mno2@mno2.org>
2015-05-05 10:39:44 +0800

    Add inplace version of merge_sort

--- sorting/merge_sort.cc
+++ sorting/merge_sort.cc
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
-void merge_sort(vector<int> &a)
+void merge_sort(std::vector<int> &a)
 {
     const int n = a.size();
 
     if (n > 1) {
-        vector<int> b(a.begin(), a.begin() + n/2);
-        vector<int> c(a.begin() + n/2, a.end());
+        std::vector<int> b(a.begin(), a.begin() + n/2);
+        std::vector<int> c(a.begin() + n/2, a.end());
 
         merge_sort(b);
         merge_sort(c);
@@ -23,9 +21,35 @@ void merge_sort(vector<int> &a)
     }
 }
 
+
+void inplace_merge(std::vector<int> &a, int i, int m, int j)
+{
+    std::vector<int> b(a.begin()+i, a.begin()+m+1);
+    std::vector<int> c(a.begin()+m+1, a.begin()+j+1);
+
+    for (int p = i, q = 0, r = 0; p <= j; p++) {
+        if (r == c.size()) a[p] = b[q++];
+        else if (q == b.size()) a[p] = c[r++];
+        else if (b[q] < c[r]) a[p] = b[q++];
+        else a[p] = c[r++];
+    }
+}
+
+
+void inplace_merge_sort(std::vector<int> &a, int i, int j)
+{
+    if (i < j) {
+        int m = (i+j) / 2;
+        inplace_merge_sort(a, i, m);
+        inplace_merge_sort(a, m+1, j);
+        inplace_merge(a, i, m+1, j);
+    }
+}
+
+
 int main()
 {
-    vector<int> a { 6, 5, 4, 3, 2, 1 };
+    std::vector<int> a { 6, 5, 4, 3, 2, 1 };
     merge_sort(a);
     for (auto elem: a) {
         std::cout << elem << ' ';
